d0dade7351de40efa82b6ad19afbe823ce434fff
daletom <tom@imgix.com>
2020-02-26 13:24:36 -0800

    Update images.js

--- src/images/images.js
+++ src/images/images.js
@@ -1,29 +1,9 @@
-const imageArray = [
-  "https://tom.imgix.net/artsy/1.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/2.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/3.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/4.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/5.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/6.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/7.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/8.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/9.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/10.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/11.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/12.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/13.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/14.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/15.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/16.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/17.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/18.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/19.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/20.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/21.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/22.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/23.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/24.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1",
-  "https://tom.imgix.net/artsy/25.jpg?w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1"
-];
+const host = "https://tom.imgix.net/artsy/";
+const imageArray = [];
+let imgixAPI = "w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1";
+let images = new Array(25);
+for (let i = 0; i < images.length; i++) {
+  imageArray.push(host + (i+1) + ".jpg?" + imgixAPI);
+};
 
 export default imageArray;

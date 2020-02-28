45f0759096be527e41d3d8f50ee1722f0b888a33
daletom <tom@imgix.com>
2020-02-28 10:27:32 -0800

    Update images.js

--- src/images/images.js
+++ src/images/images.js
@@ -1,6 +1,6 @@
 const host = "https://tom.imgix.net/artsy/";
 const imageArray = [];
-let imgixAPI = "w=600&h=600&auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1";
+let imgixAPI = "auto=format,compress&cs=tinysrgb&fit=crop&crop=faces,entropy&ar=1:1";
 let images = new Array(25);
 for (let i = 0; i < images.length; i++) {
   imageArray.push(host + (i+1) + ".jpg?" + imgixAPI);
